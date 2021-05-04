582f5a806fbefa238dd9512648b5d875132d037e
Andrii Kliuiko <andriikliuiko@Andriis-MacBook-Pro.local>
2021-05-04 21:26:16 +0300

    Updated main.

--- client/src/main/java/com/service/Main.java
+++ client/src/main/java/com/service/Main.java
@@ -1,16 +1,18 @@
 package com.service;
 
 import com.service.model.Article;
-import com.service.processor.ProcessorImpl;
-import com.service.source.Format;
+import com.service.model.NewsApiArticle;
+import com.service.model.SimpleApiArticle;
+import com.service.parser.ParserApiFacade;
+import com.service.source.Source;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import static com.service.source.Format.NEWSAPI;
-import static com.service.source.Format.SIMPLE;
+import static com.service.format.Format.NEWSAPI;
+import static com.service.format.Format.SIMPLE;
 import static com.service.source.Source.FILE;
 import static com.service.source.Source.URL;
 
@@ -20,51 +22,50 @@ public class Main {
 
     private static final String NEWS_API_KEY = "136a81fcc8e8433f8be20587cf40a7da";
 
-    private static Map<Format, String> formatUrlMap = new HashMap<>() {{
-        put(SIMPLE, "http://users.csc.calpoly.edu/~akeen/courses/csc305/simple.json");
-        put(NEWSAPI, "http://newsapi.org/v2/top-headlines?country=us&apiKey=" + NEWS_API_KEY);
+    private static Map<Source, String> newsApiFormatMap = new HashMap<>() {{
+        put(URL, "http://newsapi.org/v2/top-headlines?country=us&apiKey=" + NEWS_API_KEY);
+        put(FILE, "newsapi.json");
     }};
 
-    private static Map<Format, String> formatFileMap = new HashMap<>() {{
-        put(SIMPLE, "simple.json");
-        put(NEWSAPI, "newsapi.json");
+    private static Map<Source, String> simpleApiFormatMap = new HashMap<>() {{
+        put(URL, "http://users.csc.calpoly.edu/~akeen/courses/csc305/simple.json");
+        put(FILE, "simple.json");
     }};
 
     /**
-     * Main entry to the program. Demonstrates the ability to parse news api responses
+     * Main entry to the program. Demonstrates the ability to parse NEWS API responses
      * @param args input arguments of the program
      */
     public static void main(String[] args) {
-
-        // Retrieving json from url
-        for (Map.Entry<Format, String> urlEntrySet: formatUrlMap.entrySet()) {
-            Format format = urlEntrySet.getKey();
+        // Retrieving news api format json
+        for (Map.Entry<Source, String> urlEntrySet: newsApiFormatMap.entrySet()) {
+            Source source = urlEntrySet.getKey();
             String url = urlEntrySet.getValue();
 
             LOGGER.info("Parsing articles using provided location " + url + "\n\n");
 
-            ProcessorImpl processor = new ProcessorImpl(URL, format, url, LOGGER);
-            List<Article> articles = processor.process();
-            printResults(articles, url);
+            ParserApiFacade<NewsApiArticle> parserApiFacade = new ParserApiFacade<>(LOGGER, url, NEWSAPI, source);
+            List<NewsApiArticle> newsApiArticles = parserApiFacade.processArticles();
+            printResults(newsApiArticles, url);
         }
 
-        // Retrieving json from files
-        for (Map.Entry<Format, String> urlEntrySet: formatFileMap.entrySet()) {
-            Format format = urlEntrySet.getKey();
+        // Retrieving simple api format json
+        for (Map.Entry<Source, String> urlEntrySet: simpleApiFormatMap.entrySet()) {
+            Source source = urlEntrySet.getKey();
             String url = urlEntrySet.getValue();
 
             LOGGER.info("Parsing articles using provided location " + url + "\n\n");
 
-            ProcessorImpl processor = new ProcessorImpl(FILE, format, url, LOGGER);
-            List<Article> articles = processor.process();
-            printResults(articles, url);
+            ParserApiFacade<SimpleApiArticle> parserApiFacade = new ParserApiFacade<>(LOGGER, url, SIMPLE, source);
+            List<SimpleApiArticle> simpleApiArticles = parserApiFacade.processArticles();
+            printResults(simpleApiArticles, url);
         }
     }
 
-    private static void printResults(List<Article> articleList, String url) {
-        if (!articleList.isEmpty()) {
+    private static void printResults(List<? extends Article> newsApiArticleList, String url) {
+        if (!newsApiArticleList.isEmpty()) {
             LOGGER.info("Valid articles using provided url " + url + "\n");
-            articleList.forEach(article ->
+            newsApiArticleList.forEach(article ->
                     LOGGER.info("Valid article: \nTitle: " + article.getTitle()
                             + " , \nDescription : " + article.getDescription()
                             + " , \nUrl : " + article.getUrl()
